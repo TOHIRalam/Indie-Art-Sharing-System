e9faaad9d4010b3218f3c009737cbf1f96fed37c
Tasnimul Alam <tohiralam153@gmail.com>
2021-09-16 05:21:57 +0600

    Simple Mail Transfer Protocol(SMTP) Added

--- Controllers/SigninSignupController.cs
+++ Controllers/SigninSignupController.cs
@@ -1,11 +1,9 @@
 ﻿using Indie_Art_Sharing_System.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace Indie_Art_Sharing_System.Controllers
 {
@@ -23,8 +21,26 @@ namespace Indie_Art_Sharing_System.Controllers
             return View();
         }
 
+        [HttpGet]
         public IActionResult SignUp()
         {
+            return View();
+        }
+
+        [HttpPost, ValidateAntiForgeryToken]
+        public IActionResult Complete(User user)
+        {
+            MailMessage mailMessage = new MailMessage();
+            mailMessage.To.Add(user.Email);
+            mailMessage.From = new MailAddress("alomhero435@gmail.com");
+            mailMessage.Subject = "Verification Code";
+            mailMessage.Body = $"Hi {user.Name},\nCongratulations! You are successfully registered on Indie Arts\n\n@Regards from <span style='color: Blue'>Hero Alom</span>";
+            mailMessage.IsBodyHtml = true;
+            SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+            smtp.EnableSsl = true;
+            smtp.UseDefaultCredentials = false;
+            smtp.Credentials = new NetworkCredential("alomhero435@gmail.com", "Heroalom1122"); ;
+            smtp.Send(mailMessage);
             return View();
         }
 
